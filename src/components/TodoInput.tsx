db09441f330803d15826de053accfe357fd85b89
Panatchakorn Worachotchanapat <panatchakorn@daytech.co.th>
2021-06-09 13:16:23 +0700

    input click add todo

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TodoInput = () => {
+  const todoObj = {
+    id: 1,
+    content: "",
+    isDone: false,
+  };
+  const [todo, setTodo] = useState(todoObj);
+
+  const HandleTodoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodo({ ...todo, content: e.target.value });
+  };
+
+  const onSubmitTodo = (e: any) => { 
+    e.preventDefault();
+    console.log("todo: ", todo);
+    setTodo(todoObj);
+  };
+
   return (
     <>
       <from>
         <input
           type="text"
           name="todo"
-          value=""
+          value={todo.content}
           placeholder="What do u want to do?"
+          onChange={HandleTodoChange}
         />
-        <button type="submit">Add</button>
+        <button type="submit" onClick={onSubmitTodo}>
+          Add
+        </button>
       </from>
     </>
   );
