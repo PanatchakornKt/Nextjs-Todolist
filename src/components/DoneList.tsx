17b7cabfd80e5d90017cc6d9d4607bc366ba6165
Panatchakorn Worachotchanapat <panatchakorn@daytech.co.th>
2021-06-09 13:28:16 +0700

    todolist and donelist

--- src/components/DoneList.tsx
+++ src/components/DoneList.tsx
@@ -1,9 +1,35 @@
 import React from "react";
 
 const DoneList = () => {
+
+  const todos = [
+    {
+      id: Math.random() * 1000,
+      content: "Do Homework",
+      isDone: true,
+    },
+    {
+      id: Math.random() * 1000,
+      content: "Play game",
+      isDone: true,
+    },
+  ];
+
   return (
     <>
-      <h1>Done List</h1>
+      <div>
+        <h2>Done</h2>
+        <ul>
+          {todos.map((todo) => {
+            return (
+              <li key={todo.id}>
+                <input type="checkbox" defaultChecked={todo.isDone} disabled/>
+                {todo.content}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </>
   );
 };
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,9 +1,34 @@
 import React from "react";
 
 const TodoList = () => {
+  const todos = [
+    {
+      id: Math.random() * 1000,
+      content: "Do Homework",
+      isDone: true,
+    },
+    {
+      id: Math.random() * 1000,
+      content: "Play game",
+      isDone: false,
+    },
+  ];
+
   return (
     <>
-      <h1>TodoList</h1>
+      <div>
+      <h2>Todo</h2>
+        <ul>
+          {todos.map((todo) => {
+            return (
+              <li key={todo.id}>
+                <input type="checkbox" defaultChecked={todo.isDone} />
+                {todo.content}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </>
   );
 };
