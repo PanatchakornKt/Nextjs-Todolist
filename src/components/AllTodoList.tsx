9b7c371c1393b351975e7eb8fd9beffc9adc3b48
Panatchakorn Worachotchanapat <panatchakorn@daytech.co.th>
2021-06-11 10:19:42 +0700

    edit board

--- src/components/AllTodoList.tsx
+++ src/components/AllTodoList.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useRecoilState } from "recoil";
 import { todosState } from "@/components/AtomsState";
+import { Checkbox } from "antd";
 
 const AllTodoList = () => {
   const [todos, setTodos] = useRecoilState(todosState);
@@ -18,7 +19,10 @@ const AllTodoList = () => {
           {todos.map((todo) => {
             return (
               <div key={todo.id} className="mb-2">
-                {todo.content}
+                <label>
+                  <Checkbox defaultChecked={todo.isDone} disabled />
+                </label>
+                <label className="pl-5">{todo.content}</label>
                 <label className="text-xs text-gray-400 pl-10">
                   Add on : {todo.date}
                 </label>
